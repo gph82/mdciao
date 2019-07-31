4cf76f026dad06eec8183cee74c14b719c950b42
gph82 <guille.perez.hernandez@gmail.com>
2019-07-31 12:22:18 +0200

    [sites.py] changed argparser and other stuff

--- sites.py
+++ sites.py
@@ -6,12 +6,11 @@ from matplotlib import pyplot as plt
 from os.path import splitext, split as psplit
 from json import load as jsonload
 
-
-from actor_utils import get_fragments, \
-ctc_freq_reporter_by_residue_neighborhood, \
-    xtcs2ctcs, rangeexpand, unique_pairlist_by_tuple_hashing, interactive_fragment_picker_by_resSeq,\
+# TODO check what's going on here
+from actor_utils import get_fragments,\
+    xtcs2ctcs, rangeexpand, \
     interactive_fragment_picker_by_AAresSeq, dangerously_auto_fragments, \
-    in_what_fragment, mycolors, bonded_neighborlist_from_top, _print_frag, _replace4latex, table2BW_by_AAcode, \
+    in_what_fragment, _print_frag, _replace4latex, table2BW_by_AAcode, \
     guess_missing_BWs, _relabel_consensus, CGN_transformer, top2CGN_by_AAcode
 
 
@@ -36,9 +35,11 @@ parser.add_argument('--pbc',    dest='pbc', action='store_true', help="Consider 
 parser.add_argument('--no-pbc', dest='pbc', action='store_false')
 parser.set_defaults(pbc=True)
 
-parser.add_argument('--ask_fragment',    dest='ask', action='store_true', help="Interactively ask for fragment assignemnt when input matches more than one resSeq")
-parser.add_argument('--no-ask_fragment', dest='ask', action='store_false')
-parser.set_defaults(ask=True)
+parser.add_argument('--default_fragment_index', default=None, type=int, help="In case a residue identified as, e.g, GLU30, appears more than\n"
+                                                               " one time in the topology, e.g. in case of a dimer, the user can\n"
+                                                               " pass which fragment/monomer should be chosen by default. The\n"
+                                                               " default behaviour (None) will prompt the user when necessary")
+
 parser.add_argument('--output_npy', type=str, help="Name of the output.npy file for storing this runs' results",
                     default='output_sites')
 
@@ -78,7 +79,7 @@ else:
                                                                method="bonds",
                                                                verbose=False,
                                                                force_resSeq_breaks=True,
-                                                               frag_breaker_to_pick_idx=0,
+                                                               pick_first_fragment_by_default=True,
                                                                )
         fragment_names.extend(refgeom.top.residue(ifrag[0]).name for ifrag in fragments[len(fragment_names):])
 
@@ -117,7 +118,7 @@ def sitefile2site(sitefile):
     with open(sitefile,"r") as f:
         idict = jsonload(f)
     try:
-        idict["bonds"]["AAresSeq"]=[item.split("-") for item in idict["bonds"]["AAresSeq"]]
+        idict["bonds"]["AAresSeq"]=[item.split("-") for item in idict["bonds"]["AAresSeq"] if item[0]!='#']
         idict["n_bonds"]=len(idict["bonds"]["AAresSeq"])
     except:
         print("Malformed .json file for the site %s"%sitefile2site())
@@ -134,8 +135,10 @@ AAresSeqs = [item for sublist in AAresSeqs for item in sublist]
 AAresSeqs = [item for sublist in AAresSeqs for item in sublist]
 
 resSeq2residxs, _ = interactive_fragment_picker_by_AAresSeq(AAresSeqs, fragments,refgeom.top,
-                                                         pick_first_fragment_by_default=not a.ask,
+                                                            default_fragment_idx=a.default_fragment_index,
                                                          )
+if None in resSeq2residxs.values():
+    raise ValueError("These residues of your input have not been found. Please revise it:\n%s"%('\n'.join([key for key, val in resSeq2residxs.items() if val is None])))
 print('%10s  %6s  %7s  %10s'%tuple(("residue  residx    fragment  input_resSeq".split())))
 for key, val in resSeq2residxs.items():
     print('%10s  %6u  %7u  %s'%(refgeom.top.residue(val), val, in_what_fragment(val, fragments), key))
@@ -163,125 +166,102 @@ for isite in sites:
         isite["ctcs"].append(ctc_traj[:,isite["ctc_idxs"]])
     #print()
 
+print("The following files have been created")
+panelheight=3
+xvec = np.arange(np.max([ss["n_bonds"] for ss in sites]))
+n_cols=np.min((4, len(sites)))
+n_rows = np.ceil(len(sites)/n_cols).astype(int)
+panelsize=4
+panelsize2font=3.5
+histofig, histoax = plt.subplots(n_rows,n_cols, sharex=True, sharey=True, figsize=(n_cols*panelsize*2, n_rows*panelsize), squeeze=False)
+list_of_axes = list(histoax.flatten())
+for jax, isite in zip(list_of_axes, sites):# in np.unique([ctc_idxs_small[ii] for ii in final_look]):
+    j_av_ctcs = np.vstack(isite["ctcs"])
+    ctcs_mean = np.mean(j_av_ctcs < a.ctc_cutoff_Ang / 10, 0)
+    patches = jax.bar(np.arange(isite["n_bonds"]), ctcs_mean,
+                      # label=res_and_fragment_str,
+                      width=.25)
+    jax.set_title(
+        "Contact frequency @%2.1f $\AA$\n of site '%s'" % (a.ctc_cutoff_Ang, isite["name"]))
+    jax.set_ylim([0, 1])
+    jax.set_xlim([-.5, xvec[-1]+1 - .5])
+    jax.set_xticks([])
+    jax.set_yticks([.25, .50, .75, 1])
+    # jax.set_yticklabels([])
+    [jax.axhline(ii, color="k", linestyle="--", zorder=-1) for ii in [.25, .50, .75]]
+    #jax.legend(fontsize=panelsize * panelsize2font)
 
-#print("Will take a look at:")
-split_by_neighborhood = True
-if not split_by_neighborhood:
-    myfig, myax = plt.subplots(len(final_look), 1, sharex=True, sharey=True)
 
-    for ii, oo in enumerate(final_look):
-        pair = ctc_idxs_small[oo]
-        print([refgeom.top.residue(jj) for jj in pair])
-        plt.sca(myax[ii])
-        label = '%s-%s (%s-%s) (%u)' % (refgeom.top.residue(pair[0]), refgeom.top.residue(pair[1]),
-                                        (ctcs_mean[oo] * 100))
-        plt.plot(time_array/1e3, actcs[:, oo]*10,
-                 label=label)
-        plt.legend()
-        # plt.yscale('log')
-        plt.ylabel('d / nm')
-        plt.ylim([0, 10])
-        iax = plt.gca()
-        iax.axhline(a.ctc_cutoff_Ang, color='r')
-    iax.set_xlabel('t / ns')
-    plt.show()
-else:
-    print("The following files have been created")
-    panelheight=3
-    xvec = np.arange(np.max([ss["n_bonds"] for ss in sites]))
-    n_cols=np.min((4, len(sites)))
-    n_rows = np.ceil(len(sites)/n_cols).astype(int)
-    panelsize=4
-    panelsize2font=3.5
-    histofig, histoax = plt.subplots(n_rows,n_cols, sharex=True, sharey=True, figsize=(n_cols*panelsize*2, n_rows*panelsize), squeeze=False)
-    list_of_axes = list(histoax.flatten())
-    for jax, isite in zip(list_of_axes, sites):# in np.unique([ctc_idxs_small[ii] for ii in final_look]):
-        j_av_ctcs = np.vstack(isite["ctcs"])
-        ctcs_mean = np.mean(j_av_ctcs < a.ctc_cutoff_Ang / 10, 0)
-        patches = jax.bar(np.arange(isite["n_bonds"]), ctcs_mean,
-                          # label=res_and_fragment_str,
-                          width=.25)
-        jax.set_title(
-            "Contact frequency @%2.1f $\AA$\n of site '%s'" % (a.ctc_cutoff_Ang, isite["name"]))
-        jax.set_ylim([0, 1])
-        jax.set_xlim([-.5, xvec[-1]+1 - .5])
-        jax.set_xticks([])
-        jax.set_yticks([.25, .50, .75, 1])
-        # jax.set_yticklabels([])
-        [jax.axhline(ii, color="k", linestyle="--", zorder=-1) for ii in [.25, .50, .75]]
-        #jax.legend(fontsize=panelsize * panelsize2font)
+    # Also, prepare he timedep plot, since it'll also iterate throuth the pairs
+    myfig, myax = plt.subplots(isite["n_bonds"], 1, sharex=True, sharey=True,
+                               figsize=(10, isite["n_bonds"] * panelheight))
 
-        # Loop over the pairs to attach labels to the bars
+    myax=np.array(myax,ndmin=1)
+    myax[0].set_title("site: %s" % (isite["name"]))
 
-        # Also, prepare he timedep plot, since it'll also iterate throuth the pairs
-        myfig, myax = plt.subplots(isite["n_bonds"], 1, sharex=True, sharey=True,
-                                   figsize=(10, isite["n_bonds"] * panelheight))
+    # Loop over the pairs to attach labels to the bars
+    for ii, (iy, ipair) in enumerate(zip(ctcs_mean, isite["res_idxs"])):
+        labels_consensus = [_relabel_consensus(jj, [BW, CGN]) for jj in ipair]
+        #TODO this .item() is to comply to
+        labels_frags = [in_what_fragment(idx.item(), fragments, fragment_names=fragment_names) for idx in ipair]
+        for jj, jcons in enumerate(labels_consensus):
+            if str(jcons).lower() != "na":
+                labels_frags[jj] = jcons
 
-        myax=np.array(myax,ndmin=1)
-        myax[0].set_title("site: %s" % (isite["name"]))
-        for ii, (iy, ipair) in enumerate(zip(ctcs_mean, isite["res_idxs"])):
-            labels_consensus = [_relabel_consensus(jj, [BW, CGN]) for jj in ipair]
-            labels_frags = [in_what_fragment(idx, fragments, fragment_names=fragment_names) for idx in ipair]
-            for jj, jcons in enumerate(labels_consensus):
-                if str(jcons).lower() != "none":
-                    labels_frags[jj] = jcons
+        ilab = '%s@%s-\n%s@%s' % (
+            refgeom.top.residue(ipair[0]), labels_frags[0],
+            refgeom.top.residue(ipair[1]), labels_frags[1]
+        )
 
-            ilab = '%s@%s-\n%s@%s' % (
-                refgeom.top.residue(ipair[0]), labels_frags[0],
-                refgeom.top.residue(ipair[1]), labels_frags[1]
+        ilab = _replace4latex(ilab)
+        ix = ii
+        iy += .01
+        if iy>.65:
+            iy=.65
+        jax.text(ix-.05, iy, ilab,
+                 va='bottom',
+                 ha='left',
+                 rotation=45,
+                 fontsize=panelsize * panelsize2font,
+                 backgroundcolor="white"
+                 )
+
+        # Now the trajectory loops
+        # Loop over trajectories
+        for itime, ixtc, ictc in zip(time_array, xtcs, isite["ctcs"]):
+            plt.sca(myax[ii])
+            plt.plot(itime / 1e3,
+                     ictc[:, ii]*10,
+                     label=ixtc
+            #         ctcs_mean[oo] * 100
             )
+            plt.legend(loc=1)
 
-            ilab = _replace4latex(ilab)
-            ix = ii
-            iy += .01
-            if iy>.65:
-                iy=.65
-            jax.text(ix-.05, iy, ilab,
-                     va='bottom',
-                     ha='left',
-                     rotation=45,
-                     fontsize=panelsize * panelsize2font,
-                     backgroundcolor="white"
-                     )
-
-            # Now the trajectory loops
-            # Loop over trajectories
-            for itime, ixtc, ictc in zip(time_array, xtcs, isite["ctcs"]):
-                plt.sca(myax[ii])
-                plt.plot(itime / 1e3,
-                         ictc[:, ii]*10,
-                         label=ixtc
-                #         ctcs_mean[oo] * 100
-                )
-                plt.legend(loc=1)
-
-                plt.ylabel('d / Ang')
-                plt.ylim([0, 10])
-                iax = plt.gca()
-                iax.axhline(a.ctc_cutoff_Ang, color='r')
-            myax[ii].text(np.mean(iax.get_xlim()), .5, ilab.replace("\n", ""), ha='left')
+            plt.ylabel('d / Ang')
+            plt.ylim([0, 10])
+            iax = plt.gca()
+            iax.axhline(a.ctc_cutoff_Ang, color='r')
+        myax[ii].text(np.mean(iax.get_xlim()), .5, ilab.replace("\n", ""), ha='left')
 
 
-        iax.set_xlabel('t / ns')
-        myfig.tight_layout(h_pad=0,w_pad=0,pad=0)
+    iax.set_xlabel('t / ns')
+    myfig.tight_layout(h_pad=0,w_pad=0,pad=0)
 
-        axtop, axbottom = myax[0], myax[-1]
-        iax2 = axtop.twiny()
-        iax2.set_xticks(axbottom.get_xticks())
-        iax2.set_xticklabels(axbottom.get_xticklabels())
-        iax2.set_xlim(axbottom.get_xlim())
-        iax2.set_xlabel(axbottom.get_xlabel())
+    axtop, axbottom = myax[0], myax[-1]
+    iax2 = axtop.twiny()
+    iax2.set_xticks(axbottom.get_xticks())
+    iax2.set_xticklabels(axbottom.get_xticklabels())
+    iax2.set_xlim(axbottom.get_xlim())
+    iax2.set_xlabel(axbottom.get_xlabel())
 
-        fname = 'site.%s.%s.time_resolved.pdf'%(isite["name"].replace(" ","_"),desc_out.strip("."))
-        plt.savefig(fname,bbox_inches="tight")
-        plt.close(myfig)
-        print(fname)
-            #plt.show()
-        
-
-
-    histofig.tight_layout(h_pad=2, w_pad=0, pad=0)
-    fname = "sites_overall.%s.pdf"%desc_out.rstrip(".")
-    histofig.savefig(fname)
+    fname = 'site.%s.%s.time_resolved.pdf'%(isite["name"].replace(" ","_"),desc_out.strip("."))
+    plt.savefig(fname,bbox_inches="tight")
+    plt.close(myfig)
     print(fname)
+        #plt.show()
+
+histofig.tight_layout(h_pad=2, w_pad=0, pad=0)
+fname = "sites_overall.%s.pdf"%desc_out.rstrip(".")
+histofig.savefig(fname)
+print(fname)
 
