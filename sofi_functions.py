c0491dbfeed04cb6eb6247b0da0ca3f24b18c18f
sofi tiwari <sofitiwari@sofis-MacBook-Pro.local>
2019-09-06 11:29:11 +0200

    [sofi_functions.py] bonded_neighborlist_from_top function was copied twice. removed duplicate and now only one copy present

--- sofi_functions.py
+++ sofi_functions.py
@@ -187,36 +187,6 @@ def top2residue_bond_matrix(top, create_standard_bonds=True,
 
     return residue_bond_matrix
 
-
-def bonded_neighborlist_from_top(top, n=1):
-    residue_bond_matrix = top2residue_bond_matrix(top)
-    neighbor_list = [[ii] for ii in range(residue_bond_matrix.shape[0])]
-    for kk in range(n):
-        for ridx, ilist in enumerate(neighbor_list):
-            new_neighborlist = [ii for ii in ilist]
-            # print("Iteration %u in residue %u"%(kk, ridx))
-            for rn in ilist:
-                row = residue_bond_matrix[rn]
-                bonded = _np.argwhere(row == 1).squeeze()
-                if _np.ndim(bonded) == 0:
-                    bonded = [bonded]
-                toadd = [nn for nn in bonded if nn not in ilist and nn != ridx]
-                if len(toadd):
-                    # print("neighbor %u adds new neighbor %s:"%(rn, toadd))
-                    new_neighborlist += toadd
-                    # print("so that the new neighborlist is: %s"%new_neighborlist)
-
-            neighbor_list[ridx] = [ii for ii in _np.unique(new_neighborlist) if ii != ridx]
-            # break
-
-    # Check that the neighborlist works both ways
-    for ii, ilist in enumerate(neighbor_list):
-        for nn in ilist:
-            assert ii in neighbor_list[nn]
-
-    return neighbor_list
-
-
 def _print_frag(ii, top, iseg, **print_kwargs):
     try:
         istr = "fragment %u with %3u AAs %s(%u)-%s(%u)" % (ii, len(iseg),
