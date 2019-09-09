cf7e71e1c589822dc3da296066cc6a470ab635aa
gph82 <guille.perez.hernandez@gmail.com>
2019-09-09 12:01:47 +0200

    [setup.py] added

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(
+    name="sofi_functions",
+    version="0.1",
+    #packages="sofi_functions", # find_packages() is not really needed for now
+    packages=find_packages(),
+    install_requires=[
+    "pandas",
+    "matplotlib",
+    "mdtraj",
+    "numpy",
+    "msmtools",
+    "scipy",
+    "Bio",
+    "ipython", # These two are for one call to Gunnar_utils
+    "XlsxWriter", # 
+    ],
+    tests_require=[
+    "mock"
+    ]
+)
+
