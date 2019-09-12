109d2a31b12a091a727c46b291041d7537856dcc
sofi tiwari <sofitiwa@gmail.com>
2019-09-12 10:20:00 +0200

    [sofi_functions.aa_utils.py] Updated the docstring format

--- sofi_functions/aa_utils.py
+++ sofi_functions/aa_utils.py
@@ -1,10 +1,18 @@
 def find_AA(top, AA):
-    r"""
-    This function is used to query the index of residue based on residue name
+    """
+    This function is used to query the index of residue based on residue name.
 
-    :param top: :py:class:`mdtraj.Topology`
-    :param AA: Valid residue name to be passed as a string, example "GLU30" or "E30"
-    :return: list of res_idxs where the residue is present, so that top.residue(idx) would return the wanted AA
+    Parameters
+    ----------
+    top : :py:class:`mdtraj.Topology`
+    AA : string
+        Valid residue name to be passed as a string, example- "GLU30" or "E30"
+
+    Returns
+    -------
+    list
+        list of res_idxs where the residue is present, so that top.residue(idx) would return the wanted AA
+
     """
     code = ''.join([ii for ii in AA if ii.isalpha()])
     if len(code)==1:
@@ -15,10 +23,18 @@ def find_AA(top, AA):
         raise ValueError("The input AA %s must have an alphabetic code of either 3 or 1 letters"%AA)
 
 def int_from_AA_code(key):
-    r"""
-    This function returns the integer part from a residue name
+    """
+    This function returns the integer part from a residue name.
 
-    :param key: Residue name passed as a string, example "GLU30"
-    :return: Integer part of the residue id, example: 30 if the input is "GLU30"
+    Parameters
+    ----------
+    key : string
+        Residue name passed as a string, example "GLU30"
+
+    Returns
+    -------
+    int
+        Integer part of the residue id, example- 30 if the input is "GLU30"
+
     """
     return int(''.join([ii for ii in key if ii.isnumeric()]))
