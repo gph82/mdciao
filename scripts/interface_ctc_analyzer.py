4973d415cbc24926ff1b053867377ed76c34b96c
gph82 <guille.perez.hernandez@gmail.com>
2019-11-07 10:54:45 +0100

    minor

--- scripts/interface_ctc_analyzer.py
+++ scripts/interface_ctc_analyzer.py
@@ -1,11 +1,11 @@
 #!/home/perezheg/miniconda3/bin/python
+from sofi_functions.command_line_tools import interface
 from sofi_functions.parsers import parser_for_interface
 parser = parser_for_interface()
 a  = parser.parse_args()
 #from sofi_functions.command_line_tools import _inform_of_parser
 #_inform_of_parser(parser)
 
-from sofi_functions.command_line_tools import interface
 # Make a dictionary out ot of it and pop the positional keywords
 b = {key:getattr(a,key) for key in dir(a) if not key.startswith("_")}
 for key in ["topology","trajectories"]:
