fa19dccb5348c6e0ee7186b0202c5a2c9759fce1
gph82 <guille.perez.hernandez@gmail.com>
2019-11-26 15:03:02 +0100

    [actor_utils.geom2COMxyz] optarg residue_idxs

--- sofi_functions/actor_utils.py
+++ sofi_functions/actor_utils.py
@@ -612,10 +612,35 @@ def COM_n_from_COM_dist_triu(COM_dist_triu,
     return COM_dist_bool_int
 
 
-def geom2COMxyz(igeom):
-    masses = [_np.hstack([aa.element.mass for aa in rr.atoms]) for rr in igeom.top.residues]
-    COMs_res_time_coords = [_np.average(igeom.xyz[:, [aa.index for aa in rr.atoms], :], axis=1, weights=rmass) for rr, rmass in
-            zip(igeom.top.residues, masses)]
+def geom2COMxyz(igeom, residue_idxs=None):
+    r"""
+    Returns the time trace of the mass-weighted,
+    per-residue center-of-mass (COM) in cartesian coordinates
+
+    Parameters
+    ----------
+    igeom : :obj:`mdtraj.Trajectory`
+
+    residue_idxs : iterable, default is None
+        Residues for which the center of mass will be computed. Default
+        is to compute all residues. The excluded residues will appear
+        as np.nans in the returned value, which has the same shape
+        regardless of the input
+
+    Returns
+    -------
+    rCOMs : numpy.ndarray of shape (igeom.n_frames, igeom.n_residues,3)
+
+    """
+
+    if residue_idxs is None:
+        residue_idxs=_np.arange(igeom.top.n_residues)
+    masses = [_np.hstack([aa.element.mass for aa in rr.atoms]) for rr in
+              igeom.top.residues]
+    COMs_res_time_coords = _np.zeros((igeom.n_residues,igeom.n_frames,3))
+    COMs_res_time_coords[:,:,:] = _np.nan
+    COMs_res_time_coords[residue_idxs] = [_np.average(igeom.xyz[:, [aa.index for aa in igeom.top.residue(index).atoms], :], axis=1, weights=masses[index])
+                            for index in residue_idxs]
     COMs_time_res_coords = _np.swapaxes(_np.array(COMs_res_time_coords),0,1)
     return COMs_time_res_coords
 
