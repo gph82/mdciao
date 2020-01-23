3b5e0f551e7ef70c30992c7ed44358246de61e19
gph82 <guille.perez.hernandez@gmail.com>
2020-01-23 13:52:24 +0100

    minor

--- scripts/CGN_overview.py
+++ scripts/CGN_overview.py
@@ -6,5 +6,5 @@ import mdtraj as _md
 parser = parser_for_CGN_overview()
 a  = parser.parse_args()
 
-BW = CGN_transformer(a.CGN_PDB)
-BW.top2defs(_md.load(a.topology).top)
+CGN = CGN_transformer(a.CGN_PDB)
+CGN.top2defs(_md.load(a.topology).top)
