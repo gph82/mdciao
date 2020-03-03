8861c92f0386865336c5d54c5d02b34c97f74ca2
gph82 <guille.perez.hernandez@gmail.com>
2020-03-03 11:09:51 +0100

    [plots] new methods span_domains, RMSD_segments_vs_orientations, unify_freq_dicts and refactor some functions

--- mdciao/plots.py
+++ mdciao/plots.py
@@ -1,7 +1,8 @@
 import numpy as _np
 from matplotlib import rcParams as _rcParams
 import matplotlib.pyplot as _plt
-from .list_utils import _replace4latex
+from .list_utils import _replace4latex,  \
+    window_average_fast as _wav
 
 def plot_contact(ictc, iax,
                  color_scheme=None,
@@ -64,7 +65,6 @@ def plot_w_smoothing_auto(iax, x, y,
                           n_smooth_hw=0):
     alpha = 1
     if n_smooth_hw > 0:
-        from .list_utils import window_average_fast as _wav
         alpha = .2
         x_smooth = _wav(x, half_window_size=n_smooth_hw)
         y_smooth = _wav(y, half_window_size=n_smooth_hw)
@@ -86,40 +86,50 @@ def compare_neighborhoods(filedict,
                           anchor,
                           colordict, width=.2, figsize=(10, 5),
                           fontsize=16,
-                          substitutions=["MG", "GDP"],
                           mutations = {},
-                          plot_singles=False, stop_at=.1, scale_fig=False):
+                          plot_singles=False,
+                          freq_cutoff_val=.1,
+                          exclude=None,
+                          scale_fig=False):
 
-    from matplotlib import rcParams as _rcParams
-
-    freqs = {key: {} for key in filedict.keys()}
-    assert len(filedict) == 2
-    for key, ifile in filedict.items():
+    def neighborhood_datfile2freqws(ifile):
+        outdict = {}
         for iline in open(ifile).read().splitlines():
             try:
                 iline = iline.split()
                 freq, names = iline[0],iline[1]
-                freq = float(freq)
-                names = names.split("-")
-                name = [name for name in names if anchor not in name]
-                assert len(name)==1,name
-                name = name[0]
-                for isub in substitutions:
-                    if name.startswith(isub):
-                        name = isub
-                        break
-                for exp, pat in mutations.items():
-                    name = name.replace(pat,exp)
-                freqs[key][name] = freq
+                outdict[names]=float(freq)
             except ValueError:
                 print(iline)
                 raise
+        return outdict
 
+    def un_anchor_keys(idict, anchor,sep="-"):
+        out_dict = {}
+        for names, val in idict.items():
+            name = [name for name in names.split(sep) if anchor not in name]
+            assert len(name) == 1, name
+            out_dict[name[0]]=val
+        return out_dict
+
+    from matplotlib import rcParams as _rcParams
+
+    freqs = {key: {} for key in filedict.keys()}
+    #assert len(filedict) == 2
+
+    for key, ifile in filedict.items():
+        idict = neighborhood_datfile2freqws(ifile)
+        idict = {replace_w_dict(key, mutations):val for key, val in idict.items()}
+        idict = un_anchor_keys(idict, anchor)
+        freqs[key] = idict
+
+    """
     for key, val in freqs.items():
         print(key)
         for key, val in val.items():
             print(key,val)
         print()
+    """
 
     if plot_singles:
         myfig, myax = _plt.subplots(1, 2, sharey=True, figsize=(figsize[0] * 2, figsize[1]))
@@ -142,30 +152,33 @@ def compare_neighborhoods(filedict,
             [_plt.gca().axhline(ii, ls=":", color="k", zorder=-1) for ii in [.25, .5, .75]]
         myfig.tight_layout()
 
-    diffs = {}
-    not_common = []
-    common = []
-    for idict1 in freqs.values():
-        for idict2 in freqs.values():
-            if not idict1 is idict2:
-                not_common += list(set(idict1.keys()).difference(idict2.keys()))
-                common += list(set(idict1.keys()).intersection(idict2.keys()))
+    freqs  = unify_freq_dicts(freqs, exclude)
+    myfig, __ = plot_unified_freq_dicts(freqs,
+                                     colordict,
+                                     width=width, fontsize=fontsize, figsize=figsize)
+    _plt.text(0 - width * 2, 1.05, "%s and:" % anchor, ha="right", va="bottom")
+    _plt.gcf().tight_layout()
+    _plt.show()
 
-    common = list(_np.unique(common))
-    not_common = list(_np.unique(not_common))
-    all_keys = common + not_common
+    return myfig
 
-    print("These interaction partners are not shared:", not_common)
-    for ifreq in freqs.values():
-        for key in not_common:
-            if key not in ifreq.keys():
-                ifreq[key] = 0
+def replace_w_dict(key, pat_exp_dict):
+    for pat, exp in pat_exp_dict.items():
+        key = key.replace(pat,exp)
+    return key
 
-    _rcParams["font.size"] = 16
-
-    delta = {}
-    for ii, key in enumerate(filedict.keys()):
-        delta[key] = width * ii
+def plot_unified_freq_dicts(freqs,
+                            colordict,
+                            width=.2,
+                            figsize=(10, 5),
+                            fontsize=16,
+                            freq_cutoff_val=.1
+                            ):
+    
+    master_keys = list(freqs.keys())
+    all_keys = list(freqs[master_keys[0]].keys())
+    for mk in master_keys[1:]:
+        assert len(all_keys)==len(list(freqs[mk].keys()))
 
     mean = []
     for key in all_keys:
@@ -174,14 +187,20 @@ def compare_neighborhoods(filedict,
             imean.append(idict[key])
         mean.append(_np.mean(imean))
 
+    _rcParams["font.size"] = 16
+
+    delta = {}
+    for ii, key in enumerate(master_keys):
+        delta[key] = width * ii
+
     _rcParams["font.size"] = fontsize
-    _plt.figure(figsize=figsize)
+    myfig = _plt.figure(figsize=figsize)
     for ii, idx in enumerate(_np.argsort(mean)[::-1]):
         key = all_keys[idx]
         # print(mean[idx], key)
         for jj, (skey, sfreq) in enumerate(freqs.items()):
             if ii == 0:
-                label = skey
+                label = '%s ($\\Sigma$= %2.1f)'%(skey, _np.sum(list(sfreq.values())))
             else:
                 label = None
             _plt.bar(ii + delta[skey], sfreq[key], width=width,
@@ -190,13 +209,140 @@ def compare_neighborhoods(filedict,
                     )
             if jj == 0:
                 _plt.text(ii, 1.05, key, rotation=45)
-        if mean[idx] <= stop_at:
+        if mean[idx] <= freq_cutoff_val:
             break
-    _plt.text(0 - width * 2, 1.05, "%s and:" % anchor, ha="right", va="bottom")
     _plt.legend()
     _plt.xticks([])
     _plt.ylim(0, 1)
     _plt.xlim(0 - width, ii + width * 2)
     _plt.yticks([0, .25, .50, .75, 1])
     [_plt.gca().axhline(ii, ls=":", color="k", zorder=-1) for ii in [.25, .5, .75]]
-    _plt.gcf().tight_layout()
+    return myfig, _plt.gca()
+
+    # This will allow more than pair comparisons in the future
+def unify_freq_dicts(freqs, exclude=None, replacement_dict={}):
+
+    not_shared = []
+    shared = []
+    for idict1 in freqs.values():
+        for idict2 in freqs.values():
+            if not idict1 is idict2:
+                not_shared += list(set(idict1.keys()).difference(idict2.keys()))
+                shared += list(set(idict1.keys()).intersection(idict2.keys()))
+
+    shared = list(_np.unique(shared))
+    not_shared = list(_np.unique(not_shared))
+    all_keys = shared + not_shared
+
+    if exclude is not None:
+        print("Excluding")
+        for ikey, ifreq in freqs.items():
+            for key in shared:
+                for pat in exclude:
+                    if pat in key:
+                        ifreq.pop(key)
+                        print("%s from %s" % (key, ikey))
+                        all_keys = [ak for ak in all_keys if ak != key]
+
+    for ikey, ifreq in freqs.items():
+        for key in not_shared:
+            if key not in ifreq.keys():
+                ifreq[key] = 0
+
+    if len(not_shared)>0:
+        print("These interactions are not shared:\n%s" % (', '.join(not_shared)))
+        print("Their cummulative ctc freq is %f. " % _np.sum(
+            [[ifreq[key] for ifreq in freqs.values()] for key in not_shared]))
+
+    return freqs
+
+def RMSD_segments_vs_orientations(dict_RMSD,
+                                  segments,
+                                  orientations,
+                                  defs_residxs,
+                                  exclude_trajs=None,
+                                  bins=50,
+                                  myax=None,
+                                  panelsize=5,
+                                  colors = ["red",
+                                               "blue"],
+                                  legends=None,
+                                  dt_ps=1,
+                                  wrt="mean structure",
+                                  sharex=False,
+                                  ):
+    linestyles = ["-", "--", ":", "-."]
+    if isinstance(colors, list):
+        colors = {key:col for key, col in zip(dict_RMSD.keys(), colors)}
+    if legends is None:
+        legends = {key:key for key in dict_RMSD.keys()}
+    if exclude_trajs is None:
+        exclude_trajs = {key: [] for key in dict_RMSD.keys()}
+    if isinstance(bins, int):
+        bins = {key: bins for key in dict_RMSD.keys()}
+
+    if myax is None:
+        nrows = len(orientations)
+        ncols = len(segments) * 2
+        myfig, myax = _plt.subplots(nrows, ncols, figsize=(ncols * panelsize, nrows * panelsize),
+                                   sharey=False,
+                                   sharex=sharex,
+                                   squeeze=False,
+                                   )
+
+    def my_average(array, weights, axis=1):
+        if _np.size(weights) == 1:
+            return array
+        return _np.average(array, weights=weights, axis=axis)
+
+    for ii, (key, iRMSF) in enumerate(dict_RMSD.items()):
+        for jax, orientation in zip(myax, orientations):
+            #print(orientation)
+            for seg_key, iax, iax2 in zip(segments, jax[::2], jax[1:][::2]):
+                tohisto = [my_average(jRMSF[1][:, defs_residxs[key][seg_key]],
+                                      weights=jRMSF[3][defs_residxs[key][seg_key]],
+                                      axis=1) for (jj, jRMSF) in enumerate(iRMSF[orientation])
+                           if jj not in exclude_trajs[key]
+                           ]
+                trajlabels = [ii for ii,__ in enumerate(iRMSF[orientation]) if ii not in exclude_trajs[key]]
+
+                h, x = _np.histogram(_np.hstack(tohisto) * 10, bins=bins[key])
+                label = 'Gs-GDP %-10s (%u trajs, %2.1f $\mu$s total)' % (legends[key], len(tohisto), h.sum() * dt_ps * 1e-6)
+                iax.plot(x[:-1], h / h.max(), color=colors[key], ls='--',
+                         label=label
+                         # alpha=.50
+                         )
+                iax.legend()
+                iax.set_title("[RMSD %s]\nwrt: %s\n oriented on %s" % (seg_key, wrt, orientation))
+                iax.set_ylim([0,1])
+                [iax2.plot(_wav(_np.arange(len(th)), 5)*dt_ps*1e-6  , _wav(th, 5) * 10,
+                           color=colors[key], ls=linestyles[ii], label=trajlabels[ii]) for (ii, th) in enumerate(tohisto)]
+                iax2.legend()
+                # iax2.set_title("[RMSD GDP(t)]")
+
+    [iax.set_xlabel("RMSD / $\AA$") for iax in myax[-1][::2]]
+
+    iax.figure.tight_layout()
+    return iax.figure, myax
+
+
+def span_domains(iax, domain_dict, span_color='b', pattern=None, alternate=False):
+    if pattern is not None and isinstance(pattern, str):
+        from .parsers import match_dict_by_patterns
+        include, __ = match_dict_by_patterns(pattern, domain_dict,
+                                                            # verbose=True
+                                                            )
+
+    if alternate:
+        fac = -1
+    else:
+        fac = 1
+    idx = 1
+    get_y_pos = lambda idx, iax: {1: iax.get_ylim()[0] * .80, -1: iax.get_ylim()[-1]}[idx]
+    for cc, (key, val) in enumerate(domain_dict.items()):
+        if key in include:
+            y = get_y_pos(idx, iax)
+            # print(idx, y)
+            iax.text(_np.mean(val), y, key, ha="center")
+            iax.axvspan(_np.min(val), _np.max(val), alpha=.25, zorder=-1, color=span_color)
+            idx *= fac
