1a6dc1db06234bb9a13d52afb25f8c88da66e716
gph82 <guille.perez.hernandez@gmail.com>
2020-04-06 13:24:41 +0200

    [list_utils] removed old window_average methods

--- tests/test_lists.py
+++ tests/test_lists.py
@@ -7,7 +7,7 @@ import io
 from mdciao.list_utils import exclude_same_fragments_from_residx_pairlist, \
     unique_list_of_iterables_by_tuple_hashing, in_what_fragment, \
     does_not_contain_strings, force_iterable, is_iterable, in_what_N_fragments, rangeexpand, \
-    pull_one_up_at_this_pos, assert_min_len, assert_no_intersection, window_average_fast, window_average, \
+    pull_one_up_at_this_pos, assert_min_len, assert_no_intersection, window_average_fast, \
     put_this_idx_first_in_pair, re_warp
 
 class Test_exclude_same_fragments_from_residx_pairlist(unittest.TestCase):
@@ -198,28 +198,6 @@ class Test_window_average_fast(unittest.TestCase):
     def test_window_average_fast_half_window_size_works(self):
         assert _np.allclose(window_average_fast(_np.arange(7), half_window_size=3), _np.array([3.0]))
         assert _np.allclose(window_average_fast(_np.arange(5), half_window_size=3), _np.array([1.42857143, 1.42857143, 1.42857143]))
-
-class Test_window_average(unittest.TestCase):
-    def test_window_average_just_works(self):
-        assert _np.allclose(window_average(_np.arange(5))[0], _np.array([2.0])) #mean
-        assert _np.allclose(_np.round(window_average(_np.arange(5))[1], 2), _np.array([1.41])) #std
-
-        assert _np.allclose(window_average(_np.arange(10))[0], _np.array([2.0, 3.0, 4.0, 5.0, 6.0, 7.0])) #mean
-        assert _np.allclose(_np.round(window_average(_np.arange(10))[1], 2), _np.array([1.41, 1.41, 1.41, 1.41, 1.41, 1.41])) #std
-
-    def test_window_average_half_window_size_works(self):
-        assert _np.allclose(window_average(_np.arange(7), half_window_size=3)[0], _np.array([3.0])) #mean
-        assert _np.allclose(window_average(_np.arange(7), half_window_size=3)[1], _np.array([2.0])) #std
-
-    def test_window_average_window_should_be_less_than_input_works(self):
-        failed_assertion = False
-        try:
-            assert window_average(_np.arange(3))
-        except AssertionError:
-            failed_assertion = True
-        assert failed_assertion
-        #below assertion won't work because the function assumes that length of input array>= window size
-        #assert _np.allclose(window_average(_np.arange(5)[0], half_window_size=3), _np.array([1.42857143, 1.42857143, 1.42857143]))
 
 class Test_put_this_idx_first_in_pair(unittest.TestCase):
     def test_put_this_idx_first_in_pair_just_works(self):
