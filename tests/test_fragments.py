2b6723a5e2a703e6422bc9a9940bae9b974c9917
gph82 <guille.perez.hernandez@gmail.com>
2020-04-14 17:03:52 +0200

    [tests.test_fragments] more tests,  change in file_for_test.pdb

--- tests/test_fragments.py
+++ tests/test_fragments.py
@@ -5,55 +5,138 @@ import numpy as _np
 from unittest.mock import patch
 import mock
 from mdciao.fragments import get_fragments, \
-    interactive_fragment_picker_by_AAresSeq, interactive_fragment_picker_wip
+    interactive_fragment_picker_by_AAresSeq, interactive_fragment_picker_wip, \
+    overview, _allowed_fragment_methods, _print_frag
 from filenames import filenames
 
 import pytest
 
 test_filenames = filenames()
 
-class Test_get_fragments(unittest.TestCase):
+class Test_overview(unittest.TestCase):
+
     def setUp(self):
         self.geom = md.load(test_filenames.file_for_test_pdb)
-        self.geom_force_resSeq_breaks = md.load(test_filenames.file_for_test_force_resSeq_breaks_is_true_pdb)
 
-    # Checking for "method" argument (which are resSeq and Bonds
-    def test_get_fragments_method(self):
+    def test_just_runs(self):
+        overview(self.geom.top)
+
+    def test_select_method(self):
+        overview(self.geom.top, "resSeq")
+
+class Test_print_fragments(unittest.TestCase):
+    def setUp(self):
+        self.geom = md.load(test_filenames.file_for_test_pdb)
+        self.fragments = get_fragments(self.geom.top, verbose=False)
+
+
+    def test_just_runs(self):
+        _print_frag(0, self.geom.top, self.fragments[0])
+
+    def test_other_name(self):
+        _print_frag(0, self.geom.top, self.fragments[0], fragment_desc="blob")
+
+    def test_raises(self):
+        with pytest.raises(Exception):
+            _print_frag(0, "self.geom.top", self.fragments[0])
+
+    def test_returns(self):
+        assert isinstance(_print_frag(0, self.geom.top, self.fragments[0], return_string=True), str)
+
+
+
+
+class Test_get_fragments_methods(unittest.TestCase):
+    def setUp(self):
+        self.geom = md.load(test_filenames.file_for_test_pdb)
+
+    def test_resSeq(self):
         by_resSeq = get_fragments(self.geom.top,
-                                  verbose=True,
-                                  auto_fragment_names=True,
                                   method='resSeq')
-        by_bonds = get_fragments(self.geom.top,
-                                 verbose=True,
-                                 auto_fragment_names=True,
-                                 method='bonds')
 
         assert _np.allclose(by_resSeq[0], [0, 1, 2])
-        assert _np.allclose(by_resSeq[1], [3, 4, 5])
-        assert _np.allclose(by_resSeq[2], [6, 7])
+        assert _np.allclose(by_resSeq[1], [3, 4])
+        assert _np.allclose(by_resSeq[2], [5])
+        assert _np.allclose(by_resSeq[3], [6, 7])
+
+    def test_bonds(self):
+        by_bonds = get_fragments(self.geom.top,
+                                 method='bonds')
 
         assert _np.allclose(by_bonds[0], [0, 1, 2])
         assert _np.allclose(by_bonds[1], [3, 4, 5])
         assert _np.allclose(by_bonds[2], [6])
         assert _np.allclose(by_bonds[3], [7])
 
+    def test_resSeqplus(self):
+        by_resSeqplus = get_fragments(self.geom.top,
+                                 verbose=True,
+                                 method='resSeq+')
 
-    def test_get_fragments_join_fragments_normal(self):
+        assert _np.allclose(by_resSeqplus[0], [0,1,2])
+        assert _np.allclose(by_resSeqplus[1], [3,4,5,6,7])
+
+    def test_resSeq_bonds(self):
+        by_both = get_fragments(self.geom.top,
+                                verbose=True,
+                      method='resSeq_bonds') #method is both
+
+        assert _np.allclose(by_both[0], [0, 1, 2])
+        assert _np.allclose(by_both[1], [3, 4])
+        assert _np.allclose(by_both[2], [5])
+        assert _np.allclose(by_both[3], [6])
+        assert _np.allclose(by_both[4], [7])
+
+    def test_chains(self):
+        by_chains = get_fragments(self.geom.top,
+                                  verbose=True,
+                                  method="chains")
+
+        assert _np.allclose(by_chains[0],[0,1,2])
+        assert _np.allclose(by_chains[1],[3,4,5])
+        assert _np.allclose(by_chains[2],[6,7]), by_chains
+
+
+    def test_dont_know_method(self):
+        with pytest.raises(ValueError):
+            get_fragments(self.geom.top,verbose=True,
+                                 method='xyz')
+
+    def test_molecule_raises_not_imp(self):
+        with pytest.raises(NotImplementedError):
+            get_fragments(self.geom.top,
+                          method="molecules")
+
+    def test_molecule_resSeqplus_raises_not_imp(self):
+        with pytest.raises(NotImplementedError):
+            get_fragments(self.geom.top,
+                          method="molecules_resSeq+")
+
+
+    def test_completeness(self):
+        for imethod in _allowed_fragment_methods:
+            frags = get_fragments(self.geom.top, method=imethod)
+            assert _np.allclose(_np.hstack(frags),_np.arange(self.geom.top.n_residues))
+
+class Test_get_fragments_other_options(unittest.TestCase):
+    def setUp(self):
+        self.geom = md.load(test_filenames.file_for_test_pdb)
+        self.geom_force_resSeq_breaks = md.load(test_filenames.file_for_test_force_resSeq_breaks_is_true_pdb)
+
+    def test_join_fragments_normal(self):
         by_bonds = get_fragments(self.geom.top,
                                  join_fragments=[[1, 2]],
                                  verbose=True,
-                                 auto_fragment_names=True,
                                  method='bonds')
         assert _np.allclose(by_bonds[0], [0, 1, 2])
         assert _np.allclose(by_bonds[1], [3, 4, 5, 6])
         assert _np.allclose(by_bonds[2], [7])
 
-    def test_get_fragments_join_fragments_special_cases(self):
+    def test_join_fragments_special_cases(self):
         # Checking if redundant fragment ids are removed from the inner list for the argument "join_fragments"
         by_bonds = get_fragments(self.geom.top,
                                  join_fragments=[[1, 2, 2]],
                                  verbose=True,
-                                 auto_fragment_names=True,
                                  method='bonds')
 
         assert _np.allclose(by_bonds[0], [0, 1, 2])
@@ -65,16 +148,23 @@ class Test_get_fragments(unittest.TestCase):
             get_fragments(self.geom.top,
                           join_fragments=[[1, 2], [2, 3]],
                           verbose=True,
-                          auto_fragment_names=True,
                           method='bonds')
 
+    def test_atoms(self):
+        for imethod in _allowed_fragment_methods:
+            fragments_residxs = get_fragments(self.geom.top, method=imethod)
+            fragments_atom_idxs = get_fragments(self.geom.top, atoms=True, method=imethod)
 
-    def test_get_fragments_break_fragments_just_works(self):
+            for rfrag, afrag in zip(fragments_residxs, fragments_atom_idxs):
+                ridxs_by_atom = _np.unique([self.geom.top.atom(aa).residue.index for aa in afrag])
+                assert _np.allclose(ridxs_by_atom,rfrag)
+
+
+    def test_break_fragments_just_works(self):
         # Checking if the fragments are breaking correctly for the argument "fragment_breaker_fullresname"
         by_bonds = get_fragments(self.geom.top,
                                  fragment_breaker_fullresname=["VAL31", "GLU27"],  # two fragment breakers passed
                                  verbose=True,
-                                 # auto_fragment_names=True,
                                  method='bonds')
         assert _np.allclose(by_bonds[0], [0])
         assert _np.allclose(by_bonds[1], [1, 2])
@@ -83,58 +173,38 @@ class Test_get_fragments(unittest.TestCase):
         assert _np.allclose(by_bonds[4], [6])
         assert _np.allclose(by_bonds[5], [7])
 
-    def test_get_fragments_break_fragments_special_cases_already_breaker(self):
+    def test_break_fragments_special_cases_already_breaker(self):
         # No new fragments are created if an existing fragment breaker is passed
         by_bonds = get_fragments(self.geom.top,
                                  fragment_breaker_fullresname=["GLU30"],  # GLU30 is already a fragment breaker
                                  verbose=True,
-                                 # auto_fragment_names=True,
                                  method='bonds')
         assert _np.allclose(by_bonds[0], [0, 1, 2])
         assert _np.allclose(by_bonds[1], [3, 4, 5])
         assert _np.allclose(by_bonds[2], [6])
         assert _np.allclose(by_bonds[3], [7])
 
-    def test_get_fragments_break_fragments_special_cases_already_breaker_passed_as_string(self):
+    def test_break_fragments_special_cases_already_breaker_passed_as_string(self):
         # Also works if input is a string instead of an iterable of strings
         by_bonds = get_fragments(self.geom.top,
                                  fragment_breaker_fullresname="GLU30",  # GLU30 is already a fragment breaker
                                  verbose=True,
-                                 # auto_fragment_names=True,
                                  method='bonds')
         assert _np.allclose(by_bonds[0], [0, 1, 2])
         assert _np.allclose(by_bonds[1], [3, 4, 5])
         assert _np.allclose(by_bonds[2], [6])
         assert _np.allclose(by_bonds[3], [7])
 
-    def test_get_fragments_break_fragments_special_cases_breaker_not_present(self):
+    def test_break_fragments_special_cases_breaker_not_present(self):
         # No new fragments are created if residue id is not present anywhere
         by_bonds = get_fragments(self.geom.top,
                                  fragment_breaker_fullresname=["Glu30"],  # not a valid id
                                  verbose=True,
-                                 # auto_fragment_names=True,
                                  method='bonds')
         assert _np.allclose(by_bonds[0], [0, 1, 2])
         assert _np.allclose(by_bonds[1], [3, 4, 5])
         assert _np.allclose(by_bonds[2], [6])
         assert _np.allclose(by_bonds[3], [7])
-
-    def test_get_fragments_method_is_both(self):
-        by_both = get_fragments(self.geom_force_resSeq_breaks.top, verbose=True, #the file has GLU27 and then LYS99 instead of LYS28
-                      auto_fragment_names=True,
-                      method='both') #method is both
-
-        assert _np.allclose(by_both[0], [0, 1, 2])
-        assert _np.allclose(by_both[1], [3, 4])
-        assert _np.allclose(by_both[2], [5])
-        assert _np.allclose(by_both[3], [6])
-        assert _np.allclose(by_both[4], [7])
-
-    def test_get_fragments_dont_know_method(self):
-        with pytest.raises(AssertionError):
-            get_fragments(self.geom.top,verbose=True,
-                                 auto_fragment_names=True,
-                                 method='xyz')
 
 class Test_interactive_fragment_picker_no_ambiguity(unittest.TestCase):
 
@@ -142,7 +212,6 @@ class Test_interactive_fragment_picker_no_ambiguity(unittest.TestCase):
         self.geom = md.load(test_filenames.file_for_test_pdb)
         self.by_bonds_geom = get_fragments(self.geom.top,
                                            verbose=True,
-                                           auto_fragment_names=True,
                                            method='bonds')
 
     def test_interactive_fragment_picker_by_AAresSeq_no_ambiguous(self):
@@ -172,7 +241,6 @@ class Test_interactive_fragment_picker_with_ambiguity(unittest.TestCase):
 
         self.by_bonds_geom2frags = get_fragments(self.geom2frags.top,
                                                  verbose=True,
-                                                 auto_fragment_names=True,
                                                  method='bonds')
 
 
@@ -299,7 +367,6 @@ class Test_interactive_fragment_picker_no_ambiguity_wip(unittest.TestCase):
         self.geom = md.load(test_filenames.file_for_test_pdb)
         self.by_bonds_geom = get_fragments(self.geom.top,
                                            verbose=True,
-                                           auto_fragment_names=True,
                                            method='bonds')
 
     def test_interactive_fragment_picker_wip_no_ambiguous(self):
@@ -325,7 +392,6 @@ class Test_interactive_fragment_picker_with_ambiguity_wip(unittest.TestCase):
 
         self.by_bonds_geom2frags = get_fragments(self.geom2frags.top,
                                                  verbose=True,
-                                                 auto_fragment_names=True,
                                                  method='bonds')
 
     def test_interactive_fragment_picker_default_fragment_idx_is_none(self):
