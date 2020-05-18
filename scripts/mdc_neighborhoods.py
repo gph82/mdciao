2f5753ab2987a0b2a8e73141c2ee551745be13fa
gph82 <guille.perez.hernandez@gmail.com>
2020-05-18 23:46:56 +0200

    [scripts.mdc_neighborhoods] import _inform_of_parser

--- scripts/mdc_neighborhoods.py
+++ scripts/mdc_neighborhoods.py
@@ -1,6 +1,6 @@
 #!/home/perezheg/miniconda3/bin/python
 import numpy as np
-from mdciao.parsers import parser_for_rn
+from mdciao.parsers import parser_for_rn, _inform_of_parser
 from mdciao.command_line_tools import residue_neighborhoods
 
 # Get and instantiate parser
