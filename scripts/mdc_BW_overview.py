ea73e5910d07b694794b6e3fa7b16c28d0cfc4b0
gph82 <guille.perez.hernandez@gmail.com>
2020-05-18 16:41:19 +0200

    [scripts] BW and CGN overviews use the new _fragment_overview frm CLTs

--- scripts/mdc_BW_overview.py
+++ scripts/mdc_BW_overview.py
@@ -1,23 +1,6 @@
 #!/home/perezheg/miniconda3/bin/python
 from mdciao.parsers import parser_for_BW_overview
-from mdciao.nomenclature_utils import LabelerBW
-import mdtraj as _md
-from os import path
-from inspect import signature
+from mdciao.command_line_tools import _fragment_overview
 parser = parser_for_BW_overview()
 a  = parser.parse_args()
-
-if path.exists(a.BW_uniprot_or_file):
-    format="%s"
-else:
-    format=signature(LabelerBW)["format"].default
-
-BW = LabelerBW(a.BW_uniprot_or_file,
-               format=format,
-               write_to_disk=a.write_to_disk)
-top = _md.load(a.topology).top
-map_conlab = BW.top2map(top)
-BW.top2defs(top, map_conlab=map_conlab)
-if a.print_conlab:
-    for ii, ilab in enumerate(map_conlab):
-        print(ii, top.residue(ii),ilab)
+_fragment_overview(a,"BW")
--- scripts/mdc_CGN_overview.py
+++ scripts/mdc_CGN_overview.py
@@ -1,10 +1,6 @@
 #!/home/perezheg/miniconda3/bin/python
 from mdciao.parsers import parser_for_CGN_overview
-from mdciao.nomenclature_utils import LabelerCGN
-import mdtraj as _md
-
+from mdciao.command_line_tools import _fragment_overview
 parser = parser_for_CGN_overview()
 a  = parser.parse_args()
-
-CGN = LabelerCGN(a.CGN_PDB)
-CGN.top2defs(_md.load(a.topology).top)
+_fragment_overview(a,"CGN")
