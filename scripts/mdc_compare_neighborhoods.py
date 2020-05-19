1ad1f6d164b2ff7ac0a74d046832121d99eef142
gph82 <guille.perez.hernandez@gmail.com>
2020-05-20 00:16:51 +0200

    [scripts/mdc_compare_neighborhoods] broken, now fixed

--- scripts/mdc_compare_neighborhoods.py
+++ scripts/mdc_compare_neighborhoods.py
@@ -1,22 +1,30 @@
 #!/home/perezheg/miniconda3/bin/python
 from mdciao.parsers import parser_for_compare_neighborhoods
 from mdciao.command_line_tools import neighborhood_comparison
+from matplotlib import pyplot as plt
 
 # Get and instantiate parser
 parser = parser_for_compare_neighborhoods()
 a  = parser.parse_args()
-assert len(a.keys.split(","))==len(a.files.split(","))
-file_dict = {key: file for key, file in zip(a.keys.split(","), a.files.split(","))}
-col_dict =  {key: col for key, col in zip(a.keys.split(","), a.colors.split(","))}
+nf = len(a.files)
+if a.keys is None:
+   keys = ["file %u"%ii for ii in range(nf)]
+else:
+    assert len(a.keys.split(","))==nf
+    keys = a.keys.split(",")
+print(a.files)
+print(a.anchor,type(a.anchor))
+file_dict = {key: file for key, file in zip(keys, a.files)}
+col_dict =  {key: col for key, col in zip(keys, a.colors.split(","))}
 mut_dict = {}
-for pair in a.mutations.split(","):
-    key, val = pair.split(":")
-    mut_dict[key.replace(" ","")]=val.replace(" ","")
-print(file_dict)
-print(col_dict)
-print(mut_dict)
+if a.mutations is not None:
+    for pair in a.mutations.split(","):
+        key, val = pair.split(":")
+        mut_dict[key.replace(" ","")]=val.replace(" ","")
 
 # Call the method
-ifig = neighborhood_comparison(file_dict, a.anchor, col_dict, mutations=mut_dict)
-ifig.savefig("comp.png")
+myfig, freqs, posret = neighborhood_comparison(file_dict, col_dict,
+                                               anchor=a.anchor,
+                                               mutations_dict=mut_dict)
+plt.show()
 
