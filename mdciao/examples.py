25af0a68a17dc2c32f4d9e254e92e9cfb43084f2
gph82 <guille.perez.hernandez@gmail.com>
2020-05-23 00:31:39 +0200

    [examples.ExamplesCLTs] refactored to be easier to coverage-test

--- mdciao/examples.py
+++ mdciao/examples.py
@@ -10,56 +10,60 @@ class ExamplesCLTs(object):
 
         examples_path = path.join(mdc_path, 'examples')
         test_data_path = path.join(mdc_path, "tests", "data")
-        xtc = path.join(examples_path, "run1.1-p.stride.5.noH.xtc")
-        pdb = path.join(examples_path, "p2.noH.pdb")
+        self.xtc = path.join(examples_path, "run1.1-p.stride.5.noH.xtc")
+        self.pdb = path.join(examples_path, "p2.noH.pdb")
 
         #pdb_full = path.join(examples_path,"gs-b2ar.pdb")
         #xtc_full = path.join(examples_path,"gs-b2ar.xtc")
         #xtc, pdb = xtc_full, pdb_full
 
-        BW_file = path.join(test_data_path, "adrb2_human_full.xlsx")
-        CGN_file = path.join(test_data_path, "CGN_3SN6.txt")
-        sitefile = path.join(examples_path, "site_201.json")
+        self.BW_file = path.join(test_data_path, "adrb2_human_full.xlsx")
+        self.CGN_file = path.join(test_data_path, "CGN_3SN6.txt")
+        self.sitefile = path.join(examples_path, "site_201.json")
         if not test:
-            xtc = path.relpath(xtc, cwd)
-            pdb = path.relpath(pdb, cwd)
-            BW_file = path.relpath(BW_file, cwd)
-            CGN_file = path.relpath(CGN_file, cwd)
-            sitefile = path.relpath(sitefile, cwd)
+            self.xtc = path.relpath(self.xtc, cwd)
+            self.pdb = path.relpath(self.pdb, cwd)
+            self.BW_file = path.relpath(self.BW_file, cwd)
+            self.CGN_file = path.relpath(self.CGN_file, cwd)
+            self.sitefile = path.relpath(self.sitefile, cwd)
 
-        self._cmd_dict = {
-            "mdc_neighborhood": ["mdc_neighborhoods.py",
-                                "%s %s" % (pdb, xtc),
-                                "--resSeq_idxs 394",
-                                "--ctc_cutoff_Ang 4",
-                                "--BW_uniprot %s" % BW_file,
-                                "--CGN_PDB %s" % CGN_file,
-                                "--n_smooth_hw 1",
-                                "--table xlsx"
+    @property
+    def mdc_neighborhood(self):
+        return ["mdc_neighborhoods.py",
+                "%s %s" % (self.pdb, self.xtc),
+                "--resSeq_idxs 394",
+                "--ctc_cutoff_Ang 4",
+                "--BW_uniprot %s" % self.BW_file,
+                "--CGN_PDB %s" % self.CGN_file,
+                "--n_smooth_hw 1",
+                "--table xlsx"
+                ]
+    @property
+    def mdc_sites(self):
+        return ["mdc_sites.py ",
+                "%s %s" % (self.pdb, self.xtc),
+                " --site_files %s" % self.sitefile,
+                " --ctc_cutoff_Ang 4",
+                " --BW_uniprot %s" % self.BW_file,
+                " --CGN_PDB %s" % self.CGN_file
+                ]
+
+    @property
+    def mdc_interface(self):
+        return ["mdc_interface.py ",
+                "%s %s" % (self.pdb, self.xtc),
+                " --fragments resSeq+",
+                " --ctc_cutoff_Ang 4 ",
+                " --frag_idxs_group_1 0",
+                " --frag_idxs_group_2 3",
+                " --BW_uniprot %s" % self.BW_file,
+                " --CGN_PDB %s" % self.CGN_file
                 ],
-
-            "mdc_interface": ["mdc_interface.py ",
-                             "%s %s" % (pdb, xtc),
-                             " --fragments resSeq+",
-                             " --ctc_cutoff_Ang 4 ",
-                             " --frag_idxs_group_1 0",
-                             " --frag_idxs_group_2 3",
-                             " --BW_uniprot %s" % BW_file,
-                             " --CGN_PDB %s" % CGN_file
-                              ],
-
-            "mdc_sites": ["mdc_sites.py ",
-                         "%s %s" % (pdb, xtc),
-                         " --site_files %s" % sitefile,
-                         " --ctc_cutoff_Ang 4",
-                         " --BW_uniprot %s" % BW_file,
-                         " --CGN_PDB %s" % CGN_file
-            ]
-        }
 
     @property
     def clts(self):
-        return list(self._cmd_dict.keys())
+
+        return [attr for attr in dir(self) if attr.startswith("mdc")]
 
     def _assert_clt_exists(self, clt):
         assert clt in self.clts, "Input method %s is not in existing methods %s" % (clt, self.clts)
@@ -67,12 +71,12 @@ class ExamplesCLTs(object):
     def show(self, clt):
         self._assert_clt_exists(clt)
         print("%s example call:" % clt)
-        oneline = " ".join(self._cmd_dict[clt])
+        oneline = " ".join(self.__getattribute__(clt))
         print(oneline.replace("--", "\n--"))
         print(oneline)
 
     def run(self, clt,show=True):
         if show:
             self.show(clt)
-        oneline = " ".join(self._cmd_dict[clt])
+        oneline = " ".join(self.__getattribute__(clt))
         run(oneline.split())
