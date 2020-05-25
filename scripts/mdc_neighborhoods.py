d79be0f8bbe1ea5e788798b212ada67c40317fbe
gph82 <guille.perez.hernandez@gmail.com>
2020-05-25 12:18:50 +0200

    [scripts/mdc_neigh] adapted to new parser with --residues, deleted output.npy

--- scripts/mdc_neighborhoods.py
+++ scripts/mdc_neighborhoods.py
@@ -9,17 +9,8 @@ a  = parser.parse_args()
 
 # Make a dictionary out ot of it and pop the positional keywords
 b = {key:getattr(a,key) for key in dir(a) if not key.startswith("_")}
-for key in ["topology","trajectories","resSeq_idxs","output_npy"]:
+for key in ["topology","trajectories","residues"]:
     b.pop(key)
 
 # Call the method
-out_dict = residue_neighborhoods(a.topology, a.trajectories, a.resSeq_idxs, **b)
-
-"""
-if out_dict is not None:
-    fname = a.output_npy
-    if not fname.endswith(".npy"):
-        fname += ".npy"
-    np.save(fname,out_dict)
-    print(fname)
-"""
+out_dict = residue_neighborhoods(a.topology, a.trajectories, a.residues, **b)
