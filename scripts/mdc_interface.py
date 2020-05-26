3546a2b002b697a0dafd999811748b31ebc0b8d8
gph82 <guille.perez.hernandez@gmail.com>
2020-05-26 15:27:04 +0200

    [scripts/interface] pop fragmentify

--- scripts/mdc_interface.py
+++ scripts/mdc_interface.py
@@ -6,8 +6,11 @@ a  = parser.parse_args()
 #from mdciao.command_line_tools import _inform_of_parser
 #_inform_of_parser(parser)
 
+if not a.fragmentify:
+    a.fragment_names="None"
+
 # Make a dictionary out ot of it and pop the positional keywords
 b = {key:getattr(a,key) for key in dir(a) if not key.startswith("_")}
-for key in ["topology","trajectories"]:
+for key in ["topology","trajectories","fragmentify"]:
     b.pop(key)
 neighborhood = interface(a.topology, a.trajectories, **b)
