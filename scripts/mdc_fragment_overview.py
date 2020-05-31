84bdee69c6f1e653f24f36e10728aab21290bff0
gph82 <guille.perez.hernandez@gmail.com>
2020-05-31 12:03:16 +0200

    [scripts/mdc_fragment_overview] uses AAs

--- scripts/mdc_fragment_overview.py
+++ scripts/mdc_fragment_overview.py
@@ -5,4 +5,4 @@ import mdtraj as _md
 
 parser = parser_for_frag_overview()
 a  = parser.parse_args()
-overview(_md.load(a.topology).top, a.methods)
+overview(_md.load(a.topology).top, methods=a.methods,AAs=a.AAs)
