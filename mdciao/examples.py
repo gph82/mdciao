69d2a5dd1063f80975a1e3ef5b238ecb5f331a11
gph82 <guille.perez.hernandez@gmail.com>
2020-05-31 12:00:24 +0200

    [examplies.ExamplesCLTs] uses new module filenames

--- mdciao/examples.py
+++ mdciao/examples.py
@@ -10,19 +10,19 @@ import subprocess
 
 class ExamplesCLTs(object):
     def __init__(self, test=False):
-
-        examples_path = _path.join(mdc_path, 'examples')
-        test_data_path = _path.join(mdc_path, "tests", "data")
-        self.xtc = _path.join(examples_path, "run1.1-p.stride.5.noH.xtc")
-        self.pdb = _path.join(examples_path, "p2.noH.pdb")
+        from mdciao.filenames import filenames
+        filenames = filenames()
+        self.xtc = filenames.traj_xtc
+        self.pdb = filenames.top_pdb
 
         #pdb_full = _path.join(examples_path,"gs-b2ar.pdb")
         #xtc_full = _path.join(examples_path,"gs-b2ar.xtc")
         #xtc, pdb = xtc_full, pdb_full
 
-        self.BW_file = _path.join(test_data_path, "adrb2_human_full.xlsx")
-        self.CGN_file = _path.join(test_data_path, "CGN_3SN6.txt")
-        self.sitefile = _path.join(examples_path, "site_201.json")
+        self.BW_file = filenames.adrb2_human_xlsx
+        self.CGN_file = filenames.CGN_3SN6
+        self.sitefile = filenames.tip_json
+
         if not test:
             self.xtc = _path.relpath(self.xtc, cwd)
             self.pdb = _path.relpath(self.pdb, cwd)
