57d932dec53d0054f707cf8eb2b9df06f0e531ac
gph82 <guille.perez.hernandez@gmail.com>
2020-07-27 21:09:39 +0200

    [mdtraj] copyright and license notice extra

--- mdciao/contacts/_mdtraj.py
+++ mdciao/contacts/_mdtraj.py
@@ -1,3 +1,52 @@
+##############################################################################
+#    This file is part of mdciao.
+#
+#    mdciao is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    mdciao is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with mdciao.  If not, see <https://www.gnu.org/licenses/>.
+#
+#    Please note: The following method "compute_contacts" is a
+#    modified version of the one found in the original MDTraj
+#    Python Library, whose original authors and copyright
+#    holders are listed below.
+
+#    The modifications consist in including the indices
+#    of closest atom-pairs in the returned values. The
+#    modifications are in lines
+##############################################################################
+
+
+##############################################################################
+# MDTraj: A Python Library for Loading, Saving, and Manipulating
+#         Molecular Dynamics Trajectories.
+# Copyright 2012-2013 Stanford University and the Authors
+#
+# Authors: Christian Schwantes
+# Contributors: Robert McGibbon
+#
+# MDTraj is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 2.1
+# of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with MDTraj. If not, see <http://www.gnu.org/licenses/>.
+##############################################################################
+
 # todo check that mdtraj's license allows for this
 # opened an issue for this:
 # https://github.com/mdtraj/mdtraj/issues/1569
