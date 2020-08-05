f993a715b655fc66a9401d9a945b80aa29811fd2
gph82 <guille.perez.hernandez@gmail.com>
2020-08-05 16:39:00 +0200

    [utils.bonds] bonded_neighborlist_from_top gets verbose optarg, passed to top2residue_bond_matrix

--- mdciao/utils/bonds.py
+++ mdciao/utils/bonds.py
@@ -79,7 +79,7 @@ def _residue_bond_matrix_to_triu_bonds(residue_bond_matrix):
             bonds.append([ii, jj])
     return bonds
 
-def bonded_neighborlist_from_top(top, n=1):
+def bonded_neighborlist_from_top(top, n=1,verbose=False):
     '''
     Returns the bonded neighbors of all the residues in the topology file.
 
@@ -99,7 +99,7 @@ def bonded_neighborlist_from_top(top, n=1):
 
     #todo this is very slow and a bit of overkill if one is only interested in the
     #neighbors of one particular residue
-    residue_bond_matrix = top2residue_bond_matrix(top)
+    residue_bond_matrix = top2residue_bond_matrix(top,verbose=verbose)
     neighbor_list = [[ii] for ii in range(residue_bond_matrix.shape[0])]
     for kk in range(n):
         for ridx, ilist in enumerate(neighbor_list):
