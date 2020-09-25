5b7fc40f3e10bb16eaeb201964618c210210f227
gph82 <guille.perez.hernandez@gmail.com>
2020-09-25 15:09:41 +0200

    [scripts] adapt API calls to trajectories being optarg

--- scripts/mdc_neighborhoods.py
+++ scripts/mdc_neighborhoods.py
@@ -34,8 +34,8 @@ if not a.fragmentify:
     a.fragment_names="None"
 # Make a dictionary out ot of it and pop the positional keywords
 b = {key:getattr(a,key) for key in dir(a) if not key.startswith("_")}
-for key in ["topology","trajectories","residues", "fragmentify"]:
+for key in ["topology","residues", "fragmentify"]:
     b.pop(key)
 
 # Call the method
-out_dict = residue_neighborhoods(a.topology, a.trajectories, a.residues, **b)
+out_dict = residue_neighborhoods(a.residues, a.topology, **b)
