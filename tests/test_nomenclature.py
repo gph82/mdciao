60d20d02b43e329ba22e0348cb9cea6b66f8e5dd
gph82 <guille.perez.hernandez@gmail.com>
2020-12-18 19:23:01 +0100

    [test_nomenclature] Test_alignment_df2_conslist increase coverage

--- tests/test_nomenclature.py
+++ tests/test_nomenclature.py
@@ -811,25 +811,34 @@ class Test_conslabel2fraglabel(unittest.TestCase):
 
 class Test_alignment_df2_conslist(unittest.TestCase):
 
-    def setUp(self):
-        self.list_of_dicts = [
+    @classmethod
+    def setUpClass(cls):
+        cls.list_of_dicts = [
             {"idx_0": 0,
              "idx_1": 0,
              "match": True,
+             "AA_0": "GLU",
              "AA_1": "GLU"},
             {"idx_0": 1,
              "idx_1": 1,
              "match": False,
+             "AA_0": "LYS",
              "AA_1": "ARG"},
             {"idx_0": 2,
              "idx_1": 2,
              "match": True,
+             "AA_0": "PHE",
              "AA_1": "PHE"},
         ]
-        self.df  = DataFrame(self.list_of_dicts)
-        self.consensus_dict = {"GLU0":"3.50",
-                               "ARG1":"3.51",
-                               "PHE2":"3.52"}
+        cls.df  = DataFrame(cls.list_of_dicts)
+        cls.consensus_dict = {"GLU0": "3.50",
+                              "ARG1": "3.51",
+                              "PHE2": "3.52"}
+
     def test_works(self):
-        out_list = nomenclature.alignment_df2_conslist(self.df,self.consensus_dict)
-        self.assertListEqual(out_list, ["3.50",None,"3.52"])
+        out_list = nomenclature.alignment_df2_conslist(self.df, self.consensus_dict)
+        self.assertListEqual(out_list, ["3.50", None, "3.52"])
+
+    def test_works_nonmatch(self):
+        out_list = nomenclature.alignment_df2_conslist(self.df, self.consensus_dict, allow_nonmatch=True)
+        self.assertListEqual(out_list, ["3.50", "3.51", "3.52"])
