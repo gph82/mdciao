64f7fd4bdcadd0d62e5a3cce6ff2ead77f435cb2
gph82 <guille.perez.hernandez@gmail.com>
2021-01-11 22:58:11 +0100

    [cli.residue_neighborhoods] minor

--- mdciao/cli/cli.py
+++ mdciao/cli/cli.py
@@ -877,7 +877,7 @@ def residue_neighborhoods(residues,
         print("using the pre_computed_contact_matrix...", end="",flush=True)
         ctc_idxs=_np.array(ctc_idxs)
     else:
-        print("in the first frame of reference geom\n'%s:...'" % topology,
+        print("in the first frame of reference geom\n'%s':..." % topology,
               #end="",
               flush=True)
         ctcs, ctc_idxs = _md.compute_contacts(refgeom[0], _np.vstack(ctc_idxs), periodic=pbc)
