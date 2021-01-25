87aa6672a8903c07e691774b42a2b0537501bb0a
gph82 <guille.perez.hernandez@gmail.com>
2021-01-25 13:03:38 +0100

    [utils] minor output formatting

--- mdciao/utils/str_and_dict.py
+++ mdciao/utils/str_and_dict.py
@@ -263,7 +263,7 @@ def unify_freq_dicts(freqs,
 
     if len(not_shared)>0:
         print("These interactions are not shared:\n%s" % (', '.join(not_shared)))
-        print("Their cumulative ctc freq is %f. " % _np.sum(
+        print("Their cumulative ctc freq is %3.2f. " % _np.sum(
             [[ifreq[key] for ifreq in freqs_work.values()] for key in not_shared]))
 
     return freqs_work
