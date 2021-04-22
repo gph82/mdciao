de56605b88843fcbd733daa870c858d8be44c4ff
gph82 <guille.perez.hernandez@gmail.com>
2021-04-22 21:32:57 +0200

    [setup] typo in email

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ doc_deps = [
 setup(
     name="mdciao",
     version="0.0.1.rc2",
-    author_email="guilleremo.perez@charite.de",
+    author_email="guillermo.perez@charite.de",
     description="mdciao: Analysis of Molecular Dynamics Simulations Using Residue Neighborhoods",
     url="https://github.com/gph82/mdciao",
     project_urls={
