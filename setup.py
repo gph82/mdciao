8ba3fc0f52adb4729e2d3b56da5cc0b7461989dc
gph82 <guille.perez.hernandez@gmail.com>
2021-04-26 17:06:18 +0200

    [setup] bump beta version, classifiers are now list, not set

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ doc_deps = [
 
 setup(
     name="mdciao",
-    version="0.0.1.beta1",
+    version="0.0.1.beta2",
     author_email="guillermo.perez@charite.de",
     description="mdciao: Analysis of Molecular Dynamics Simulations Using Residue Neighborhoods",
     url="https://github.com/gph82/mdciao",
@@ -30,7 +30,7 @@ setup(
     python_requires=">=3.6",
     long_description=long_description,
     long_description_content_type="text/x-rst",
-    classifiers = {"Development Status :: 4 - Beta",
+    classifiers = ["Development Status :: 4 - Beta",
                    "Programming Language :: Python :: 3",
                    "Operating System :: OS Independent",
                    "Intended Audience :: Science/Research",
@@ -38,7 +38,7 @@ setup(
                    "Topic :: Scientific/Engineering :: Chemistry",
                    "Topic :: Scientific/Engineering :: Bio-Informatics",
                    "Topic :: Scientific/Engineering :: Visualization",
-                   },
+                   ],
     packages=find_packages(),
     install_requires=[
                     "cython",
