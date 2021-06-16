6dde75708a299dd33f6a0849e92f7f61b0b28882
gph82 <guille.perez.hernandez@gmail.com>
2021-06-16 17:53:14 +0200

    [cli._fragment_overview] BW --> GPCR, updated parser_for_BW_overview, and script mdc_BW_overview

--- scripts/mdc_BW_overview.py
+++ scripts/mdc_BW_overview.py
@@ -26,4 +26,4 @@ from mdciao.parsers import parser_for_BW_overview
 from mdciao.cli.cli import _fragment_overview
 parser = parser_for_BW_overview()
 a  = parser.parse_args()
-_fragment_overview(a,"BW")
+_fragment_overview(a,"GPCR")
