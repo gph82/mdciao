9ce8c010686e55b8c4ab2b3536346394315ed109
gph82 <guille.perez.hernandez@gmail.com>
2021-09-29 17:49:15 +0200

    [plots.plots._try_colormap_string] distinguish between qualitative colormaps and the rest

--- mdciao/plots/plots.py
+++ mdciao/plots/plots.py
@@ -386,9 +386,13 @@ def _try_colormap_string(colors, N):
             "Your input colors string '%s' is not a matplotlib colormap.\n Check https://matplotlib.org/stable/tutorials/colors/colormaps.html"%colors)
         raise e
     if cmap.N >= N:
-        return cmap(_np.linspace(0,1,N))[:, :-1].tolist()
+        if cmap.N > 20:
+            interp = _np.linspace(0, 1, N)
+        else:
+            # These are the qualititave colormaps you  don't want to interpolate
+            interp = _np.arange(N)
+        return cmap(interp)[:, :-1].tolist()
     else:
-        #TODO: this cycles back once cmap.N has been reached, we could linspace like above
         return _np.vstack([cmap(_np.arange(cmap.N)) for ii in range(_np.ceil(N/cmap.N).astype(int))])[:N,:-1].tolist()
 
 
