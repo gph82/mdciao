721ae50b0749b9e5eeacb612f45ec1b08a49c7ea
gph82 <guille.perez.hernandez@gmail.com>
2021-10-06 16:16:46 +0200

    typos

--- mdciao/fragments/fragments.py
+++ mdciao/fragments/fragments.py
@@ -689,7 +689,7 @@ def check_if_subfragment(sub_frag, fragname, fragments, top,
 
     Example
     -------
-    Let's assume the GCPR-nomenclature tells us that TM6 is [0,1,2,3]
+    Let's assume the GPCR-nomenclature tells us that TM6 is [0,1,2,3]
     and we have already divided the topology into fragments
     using :obj:`get_fragments`, with method "resSeq+", meaning
     we have fragments for the receptor, Ga,Gb,Gg
