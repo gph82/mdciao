473b5370024c15b2ad53edecc5860fadd4acf0bd
gph82 <guille.perez.hernandez@gmail.com>
2022-05-10 11:31:55 +0200

    [cli.sites] pass new variable top2confrag to the optarg consensus_fragnames of ContactPair
    consensus_fragnames was unused so far in cli.sites, but it's very useful
    to give at least labels like "ICL3" when no consensus labels are there

--- mdciao/cli/cli.py
+++ mdciao/cli/cli.py
@@ -1781,13 +1781,18 @@ def sites(site_inputs,
     # TODO decide if/to expose _fragments_strings_to_fragments or refactor it elsewhere
     fragments_as_residue_idxs, user_wants_consensus = _mdcfrg.fragments._fragments_strings_to_fragments(fragments, refgeom.top, verbose=True)
     fragment_names = _parse_fragment_naming_options(fragment_names, fragments_as_residue_idxs)
-    fragment_defs, consensus_maps, __ = \
+    consensus_frags, consensus_maps, __ = \
         _parse_consensus_options_and_return_fragment_defs({"GPCR": GPCR_uniprot,
                                                            "CGN": CGN_PDB},
                                                           refgeom.top,
                                                           fragments_as_residue_idxs,
                                                           accept_guess=accept_guess,
                                                           save_nomenclature_files=save_nomenclature_files)
+
+    top2confrag = _np.full(refgeom.top.n_residues, None)
+    for key, val in consensus_frags.items():
+        top2confrag[val] = key
+
     sites = [_mdcsites.x2site(ff) for ff in site_inputs]
     ctc_idxs_small, site_maps = _mdcsites.sites_to_res_pairs(sites, refgeom.top,
                                                              fragments=fragments_as_residue_idxs,
@@ -1836,7 +1841,8 @@ def sites(site_inputs,
                                                fragment_idxs=fragment_idxs,
                                                fragment_names=[fragment_names[idx] for idx in fragment_idxs],
                                                atom_pair_trajs=[itraj[:, [idx * 2, idx * 2 + 1]] for itraj in
-                                                                at_pair_trajs]
+                                                                at_pair_trajs],
+                                               consensus_fragnames=[top2confrag[idx] for idx in pair]
                                                #colors=[fragcolors[idx] for idx in idxs]
                                                ))
         site_as_gc[key] = _mdcctcs.ContactGroup(site_as_gc[key], name="site '%s'"%key)
