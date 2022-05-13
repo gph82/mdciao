4055b2a987daced4b9dc37eb492f935df67b0a6b
gph82 <guille.perez.hernandez@gmail.com>
2022-05-13 20:42:51 +0200

    [filenames.Filenames] refactor RSCB_pdb_path -> RCSB_pdb_path

--- mdciao/filenames/filenames.py
+++ mdciao/filenames/filenames.py
@@ -53,7 +53,7 @@ class FileNames(object):
             self.notebooks_path = _path.join(sfpath,"mdciao","examples")
 
         self.bogus_pdb_path = _path.join(self.test_data_path, "bogus_pdb")
-        self.RSCB_pdb_path =  _path.join(self.test_data_path,"RSCB_pdb" )
+        self.RCSB_pdb_path =  _path.join(self.test_data_path, "RSCB_pdb")
         self.example_path =   _path.join(self.test_data_path,"examples")
         self.nomenclature_path = _path.join(self.test_data_path,"nomenclature")
         self.json_path = _path.join(self.test_data_path,"json")
@@ -74,9 +74,9 @@ class FileNames(object):
         self.ions_and_water = _path.join(self.bogus_pdb_path, "water_and_ions.pdb.gz")
 
         # Pure-PDBs
-        self.pdb_3CAP = _path.join(self.RSCB_pdb_path,"3cap.pdb.gz")
-        self.pdb_1U19 = _path.join(self.RSCB_pdb_path,"1u19.pdb.gz")
-        self.pdb_3SN6 = _path.join(self.RSCB_pdb_path,"3SN6.pdb.gz")
+        self.pdb_3CAP = _path.join(self.RCSB_pdb_path, "3cap.pdb.gz")
+        self.pdb_1U19 = _path.join(self.RCSB_pdb_path, "1u19.pdb.gz")
+        self.pdb_3SN6 = _path.join(self.RCSB_pdb_path, "3SN6.pdb.gz")
 
         # Traj
         self.top_pdb = _path.join(self.example_path,"gs-b2ar.noH.pdb")
