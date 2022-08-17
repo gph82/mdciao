5b63c2b539cb80db81c10d84b9d476def2b7a12e
gph82 <guille.perez.hernandez@gmail.com>
2022-08-17 18:28:14 +0200

    [examples.__init__] added KLIFSLabeler_P31751

--- mdciao/examples/__init__.py
+++ mdciao/examples/__init__.py
@@ -12,8 +12,9 @@ Helper functions for demos and data download
    Interface_B2AR_Gas
    GPCRLabeler_ardb2_human
    CGNLabeler_3SN6
+   KLIFSLabeler_P31751
    notebooks
    fetch_example_data
    filenames
 """
-from .examples import ContactGroupL394, Interface_B2AR_Gas, GPCRLabeler_ardb2_human, CGNLabeler_3SN6, filenames, notebooks, fetch_example_data
+from .examples import ContactGroupL394, Interface_B2AR_Gas, GPCRLabeler_ardb2_human, CGNLabeler_3SN6, filenames, notebooks, fetch_example_data, KLIFSLabeler_P31751
