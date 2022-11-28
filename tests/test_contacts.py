75d387b683a0843cc654c33932315690f36daf5f
gph82 <guillermo.perez@charite.de>
2022-11-28 16:13:32 +0100

    [test_contacts.TestContactGroupFrequencies] CGN_PDB -> CGN_uniprot

--- tests/test_contacts.py
+++ tests/test_contacts.py
@@ -1684,7 +1684,7 @@ class TestContactGroupFrequencies(TestBaseClassContactGroup):
         cls.GPCR = examples.GPCRLabeler_ardb2_human()
         cls.CGN = examples.CGNLabeler_3SN6()
         cls.intf = examples.examples.Interface_B2AR_Gas(GPCR_uniprot = cls.GPCR,
-                                                        CGN_PDB = cls.CGN)
+                                                        CGN_uniprot = cls.CGN)
         cls.total_intf_freq_at_3 = cls.intf.frequency_per_contact(3.0).sum()
         assert cls.total_intf_freq_at_3 > 0
     def test_frequency_dicts(self):
