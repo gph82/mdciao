3669e4691f905cb2f52444ad761669e498318ea1
gph82 <guillermo.perez@charite.de>
2024-08-27 19:45:02 +0200

    [contacts.ContactGroup._args2df] "self interface residx" only if interface

--- mdciao/contacts/contacts.py
+++ mdciao/contacts/contacts.py
@@ -7922,13 +7922,14 @@ def _full_color_list(top, df, colors=None) -> _DF:
     if colors is None:
         _colors = list(_mdcplots.color_dict_guesser("tab10", _np.arange(len(frags_from_df))).values())
         jdf["frag_color"] = list(_mdcflare._utils.col_list_from_input_and_fragments(_colors, frags_from_df))
-        one_intf_frag = _np.unique(jdf["interface fragment"])
-        if len(one_intf_frag)==1:
-            assert jdf["self interface residx"].any(), ValueError("If there's only one interface fragment, "
-                                                                  "then there should be some shared residues between interface members")
-        #For the purposes of the flareplot we'll split the shared residxs
-        jdf.loc[jdf["self interface residx"], "interface fragment"] = {1 : 0,
-                                                                       0 : 1}[one_intf_frag[0]]
+        if "interface fragment" in df.keys():
+            one_intf_frag = _np.unique(jdf["interface fragment"])
+            if len(one_intf_frag)==1:
+                assert jdf["self interface residx"].any(), ValueError("If there's only one interface fragment, "
+                                                                      "then there should be some shared residues between interface members")
+            #For the purposes of the flareplot we'll split the shared residxs
+            jdf.loc[jdf["self interface residx"], "interface fragment"] = {1 : 0,
+                                                                           0 : 1}[one_intf_frag[0]]
 
     else:
         jdf["frag_color"] = list(_mdcflare._utils.col_list_from_input_and_fragments(colors, frags_from_df))
