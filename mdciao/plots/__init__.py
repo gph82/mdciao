2b381437f239117763d53cf0dd9daf63afdf2f3b
gph82 <guillermo.perez@charite.de>
2025-01-11 17:31:59 +0100

    [plots.__init__] add histogram_w_smoothing_auto to public methods

--- mdciao/plots/__init__.py
+++ mdciao/plots/__init__.py
@@ -5,10 +5,12 @@ Plotting functions
    :toctree: generated/
 
    compare_groups_of_contacts
-   plot_matrix
+   compare_violins
    plot_unified_freq_dicts
    plot_unified_distro_dicts
-   compare_violins
+   plot_w_smoothing_auto
+   histogram_w_smoothing_auto
+   plot_matrix
    color_dict_guesser
 
 """
